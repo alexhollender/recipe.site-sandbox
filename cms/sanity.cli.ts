aed776a9250a0d8ae6d59096c6800164486e00f7
Conor Davidson <conor@conordavidson.com>
2024-04-02 14:57:15 -0400

    Add CI

--- cms/sanity.cli.ts
+++ cms/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: 'r6z49mh7',
-    dataset: 'production'
-  }
-})
+    dataset: 'production',
+  },
+});
